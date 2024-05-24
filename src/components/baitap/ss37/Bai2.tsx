947b35c17d3fc8b53abbad6b30c27fe4c4fb6323
DungVPNN <dungbgbg12tt@gmail.com>
2024-05-24 07:11:10 +0700

    ss37+38

---
+++ src/components/baitap/ss37/Bai2.tsx
@@ -0,0 +1,20 @@
+// Bai2.tsx
+import React, { useReducer, useContext } from 'react';
+import { Bai1 } from './Bai1';
+
+const reducer = (state: string, action: { type: string }) => {
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return 'blue'; 
+    default:
+      return state;
+  }
+};
+export default function Bai2() {
+  const { state, setState } = useContext(Bai1);
+  const [color, dispatch] = useReducer(reducer, state);
+  return (
+    <Bai1.Provider value={{ state: color, setState: dispatch }}>
+    </Bai1.Provider>
+  );
+}
